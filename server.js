ab13cb46bf1937dc4b6c4773ba3d7e6fc7e40bba
delmarNK <delmarjinku3@gmail.com>
2023-11-30 21:05:21 +0800

    refactor to MVC layout

--- server.js
+++ server.js
@@ -2,33 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
-const products =[
-    {
-        name: 'shirt',
-        price: 40,
-        type: "polo men's shirt",
-        img: "https://www.charlestyrwhitt.com/dw/image/v2/AAWJ_PRD/on/demandware.static/-/Sites-ctshirts-master/default/dw2934bc4a/hi-res/CSR2036NAV_MODEL_UNTUCKED.jpg?sw=430&sh=540"
-    },
-    {
-        name: 'pant',
-        price: 20,
-        type: "Jean LEVIS",
-        img: "https://resources.mandmdirect.com/Images/_default/p/l/3/pl30153_1_cloudzoom.jpg"
-    },
-    {
-        name: 'phone',
-        price: 900,
-        type: "iphone 15",
-        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-finish-select-202209-6-1inch-midnight?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1661026579503"
-    }]
 
-app.get('/products', (req,res)=>{
-    res.send(products)
-})
+const productsController =require('./controllers/products_controller')
 
-app.get('/products/:id', (req,res)=>{
-    res.send(products[req.params.id])
-})
+app.use('/products', productsController)
+
 
 
 app.listen(PORT,()=>{
